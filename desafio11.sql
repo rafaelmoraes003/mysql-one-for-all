2dd4682654759108f88e074e4a47ae7adbaed547
rafael moraes <rafaelsm003@gmail.com>
2022-07-23 02:53:31 -0300

    feat: exibe nomes de musicas em sua forma normal e com string trocada

---
+++ desafio11.sql
@@ -0,0 +1,19 @@
+SELECT 
+name AS nome_musica,
+(
+	CASE
+  		WHEN name LIKE '%Streets' THEN REPLACE(name, 'Streets', 'Code Review')
+  		WHEN name LIKE '%Her Own' THEN REPLACE(name, 'Her Own', 'Trybe')
+  		WHEN name LIKE '%Inner Fire' THEN REPLACE(name, 'Inner Fire', 'Project')
+  		WHEN name LIKE '%Silly' THEN REPLACE(name, 'Silly', 'Nice')
+  		WHEN name LIKE '%Circus' THEN REPLACE(name, 'Circus', 'Pull Request')
+  	END
+) AS novo_nome
+FROM Songs
+WHERE 
+name LIKE '%Streets'
+OR name LIKE '%Her Own'
+OR name Like '%Circus'
+OR name Like '%Silly'
+OR name LIKE '%Inner Fire'
+ORDER BY nome_musica;
